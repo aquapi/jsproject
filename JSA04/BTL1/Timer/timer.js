a0a9a4b3ffa75d5ba95cbe947a3614bd85a4852a
aquapi <aquaplmc@gmail.com>
2021-08-06 19:40:52 +0700

    JSProject

--- JSA04/BTL1/Timer/timer.js
+++ JSA04/BTL1/Timer/timer.js
@@ -6,7 +6,7 @@
 function clock(time, selector) {
     var k = time, sec = time % 60, min = (time - sec) / 60 % 60, hour = (time - sec - min * 60) / 3600 % 60, secstr, minstr, hourstr;
     for (let i = 0; i < time + 1; i++) {
-        setTimeout(function() {
+        setTimeout(function () {
             if (sec === -1) return;
             (sec < 10) ? secstr = "0" + sec : secstr = sec;
             (min < 10) ? minstr = "0" + min : minstr = min;
@@ -15,5 +15,5 @@ function clock(time, selector) {
             k--;
             sec = k % 60, min = (k - sec) / 60 % 60, hour = (k - min * 60 - sec) / 3600 % 60;
         }, 1000);
-    }   
+    }
 }
