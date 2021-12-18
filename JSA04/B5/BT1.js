b2e286a281a159b87e447d2444feb82b6afdc899
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-18 09:18:48 +0700

    Delete JSA04 directory

--- JSA04/B5/BT1.js
+++
@@ -1,7 +0,0 @@
-function Student(name, age, address) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-}
-
-console.log(JSON.stringify(new Student("James", 16, "james@gmail.com")));
--- JSA04/B5/BT3.js
+++
@@ -1,3 +0,0 @@
-const foo = (x, y, z) => console.log(x, y, x);
-const foo1 = () => alert("Hello");
-const foo2 = (a, b) => a + b * 100;
